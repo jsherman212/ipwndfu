79120150e768452c89a4cb82f27b28adeb49ceef
jsherman212 <justinsherman419@yahoo.com>
2021-08-07 11:41:32 -0400

    remove SecureDBG folder

--- SecureDBG/init.h
+++
@@ -1,8 +0,0 @@
-#ifndef INIT
-#define INIT
-
-#include <stdbool.h>
-
-bool init(void);
-
-#endif
--- SecureDBG/panic.h
+++
@@ -1,6 +0,0 @@
-#ifndef PANIC
-#define PANIC
-
-__attribute__ ((noreturn)) void _panic(const char *, ...);
-
-#endif
